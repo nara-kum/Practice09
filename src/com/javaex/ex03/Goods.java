466c394dfc1c9f4edc6c45dffe8097c38efc650b
nara-kum <0603skfk@gmail.com>
2025-06-05 12:51:29 +0900

    Practice09 완

--- src/com/javaex/ex03/Goods.java
+++ src/com/javaex/ex03/Goods.java
@@ -4,17 +4,41 @@ public class Goods {
 
 	//필드
 	private String name;
-	private int price;
+	private String price;
 	private int count;
 
 	//생성자
 	public Goods() {
 	}
-	public Goods(String name, int price, int count) {
+	public Goods(String name, String price, int count) {
 		this.name = name;
 		this.price = price;
 		this.count = count;
 	}
 	
+	//메소드-gs
+	public String getName() {
+		return name;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+	//메소드일반
+	public void income() {
+		System.out.println(this.name+"(가격:"+this.price+")이 "+this.count+"개 입고 되었습니다.");
+	}
 
 }
--- src/com/javaex/ex03/GoodsApp.java
+++ src/com/javaex/ex03/GoodsApp.java
@@ -9,23 +9,35 @@ public class GoodsApp {
     public static void main(String[] args) {
     	
     	Scanner sc = new Scanner(System.in);
-    	System.out.println("상품을 입력해주세요(종료q");
+    	System.out.println("상품을 입력해주세요(종료q)");
     	
     	List<Goods> gList = new ArrayList<Goods>();
+    	int total = 0;
     	
     	while(true) {
     		
     		String in = sc.nextLine();
-    		String[] iArray = in.split(",");
     		
     		if("q".equals(in)) {
+    			System.out.println("[입력완료]");
     			break;
     		}
     		
+    		String[] iArray = in.split(",");
+    		Goods gs = new Goods(iArray[0],iArray[1],Integer.parseInt(iArray[2]));
+    		gList.add(gs);
     		
     	}
     	
+    	System.out.println("==============================");
+    	for(int i=0; i<gList.size(); i++) {
+    		gList.get(i).income();
+    	}
+    	for(int i=0; i<gList.size(); i++) {
+    		total = total + gList.get(i).getCount();
+    	}
     	
+    	System.out.println("모든 상품의 갯수는 "+total+"개입니다.");
     	sc.close();
        
     }
